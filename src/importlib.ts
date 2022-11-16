d926fb1f15c93047487a94d8c8cc27aa41ff9d4b
MokiyCodes <mokiycodes@users.noreply.github.com>
2022-11-16 19:15:14 +0100

    fix: overwrite console.log

--- src/importlib.ts
+++ src/importlib.ts
@@ -1,8 +1,10 @@
 // @ts-nocheck
 import lib from './libunasm/dist/インデックス'
 window.libunasm = lib;
+console.rawLog = console.log;
 lib.instructions.out = ()=>document.querySelector('#out').innerHTML+=(lib.registers.r1.toString());
-lib.instructions.outc = ()=>document.querySelector('#out').innerHTML+=(String.fromCharCode(lib.registers.r1));
+lib.instructions.outc = () => document.querySelector('#out').innerHTML += (String.fromCharCode(lib.registers.r1));
+console.log = (...a:string[]) => document.querySelector('#out').innerHTML += a.join(' ')+'\n'
 window.exec = (data: string) => {
   document.querySelector('#out').innerHTML = '';
   const res = libunasm.execute(data)
